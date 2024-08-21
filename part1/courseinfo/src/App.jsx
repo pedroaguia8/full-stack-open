a9a8b3b1a5bff2d8c956384fafdd90d0f45643fb
pedroaguia8 <24630163+pedroaguia8@users.noreply.github.com>
2024-08-21 04:02:52 +0100

    Refactor exercise 1.5
    This way the code is more organizes as the Part
    React component only receives one argument for
    each part.

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -36,9 +36,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <>
-      <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
-      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </>
   )
 }
@@ -47,7 +47,7 @@ const Part = (props) => {
   return (
     <>
       <p>
-        {props.name} {props.exercises}
+        {props.part.name} {props.part.exercises}
       </p>
     </>
   )
