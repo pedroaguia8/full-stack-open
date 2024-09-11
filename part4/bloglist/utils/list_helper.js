4431c615e1690e175bcba5643fb67fb2a4713f29
pedroaguia8 <24630163+pedroaguia8@users.noreply.github.com>
2024-09-11 12:19:05 +0100

    Solve exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,10 +1,21 @@
 
 
-const dummy = (blogs) => {
+const dummy = () => {
     return 1;
 }
 
+
+const totalLikes = blogs => {
+    const reducer = (sum, item) => {
+        return sum + item.likes
+    }
+
+    return blogs.length === 0
+        ? 0
+        : blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-    dummy
+    dummy, totalLikes
 }
 
